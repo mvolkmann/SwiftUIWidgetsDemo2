a154f1d7d7b48b719d530416c4fd82fd78030edf
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-09-10 14:56:58 -0500

    Initial Commit

---
+++ SwiftUIWidgetsDemo2/SwiftUIWidgetsDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWidgetsDemo2App.swift
+//  SwiftUIWidgetsDemo2
+//
+//  Created by Mark Volkmann on 9/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWidgetsDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
